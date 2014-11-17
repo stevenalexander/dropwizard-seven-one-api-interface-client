6c846b0019e901a8881c2d9bcf69c62f19a1be35
Steven Alexander <steven.william.alexander@googlemail.com>
2014-11-17 17:03:05 +0000

    added API project

---
+++ api/src/test/java/com/example/api/PersonTests.java
@@ -0,0 +1,60 @@
+package com.example.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.junit.Assert.assertEquals;
+
+public class PersonTests {
+
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+    private static final String NULL_ERROR_MESSAGE = "may not be null";
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void serializesToJson() throws Exception {
+        final Person person = getPerson();
+        assertEquals(fixture("fixtures/person.json"), MAPPER.writeValueAsString(person));
+    }
+
+    @Test
+    public void deserializesFromJSON() throws Exception {
+        final Person person = getPerson();
+        assertEquals(person, MAPPER.readValue(fixture("fixtures/person.json"), Person.class));
+    }
+
+    // Should be replaced with individual class field validator tests
+    @Test
+    public void validate_not_null() throws Exception {
+        Person person = new Person();
+
+        Set<ConstraintViolation<Person>> constraintViolations =
+                validator.validate(person);
+
+        assertEquals(2, constraintViolations.size());
+        assertEquals(NULL_ERROR_MESSAGE, constraintViolations.iterator().next().getMessage());
+    }
+
+    public static Person getPerson() {
+        return new Person()
+                .setId(10)
+                .setName("person10");
+    }
+}
